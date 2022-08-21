cd00090ff8fc8dee054f4bb732eaf8739d6db024
yyyyjinying <1020066395@qq.com>
2022-08-21 22:06:48 +0800

    edit file

--- linux/docker/docker-compose/mysql/part01/start.sh
+++ linux/docker/docker-compose/mysql/part01/start.sh
@@ -6,5 +6,10 @@ docker compose rm -f
 rm -rf /root/sorf/mysql
 mkdir -p /root/sorf/mysql/conf.d
 mkdir -p /root/sorf/mysql/data
+
+rm -rf /root/sorf/redis
+mkdir -p /root/sorf/redis/conf
+mkdir -p /root/sorf/redis/data
+
 docker compose up -d
 #docker exec -i mysql_container sh -c 'exec mysql -uroot -p"123456"' < ./all-databases.sql
